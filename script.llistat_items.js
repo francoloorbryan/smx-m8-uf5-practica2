ad6dac96d317f357724e739543a2501293291c7b
Bryan Franco <francoloorbyran@elpuig.xeill.net>
2020-10-16 17:38:21 +0200

    jikbhhik

--- script.llistat_items.js
+++ script.llistat_items.js
@@ -1,7 +1,7 @@
 const list = document.createElement('ul');
 const info = document.createElement('p');
 
-info.textContent = 'A sota hi ha una llista dinàmica. Clica aquí \u{1f4cc} per a afegir un nou item a la llista. Clica un item existent per a canviar el seu text.';
+info.textContent = 'A sota hi ha una llista dinamica. Clica aqui \u{1f4cc} per a afegir un nou item a la llista. Clica un item existent per a canviar el seu text.';
 
 document.body.appendChild(info);
 document.body.appendChild(list);
